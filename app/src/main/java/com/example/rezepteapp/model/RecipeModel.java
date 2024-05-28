b05a8536b8704248b74fcb8cdee84221139c2687
Jost <jostvanheiden03@gmail.com>
2024-05-28 18:02:47 +0200

    ToShoppingListMapper.java entfernt

--- app/src/main/java/com/example/rezepteapp/model/RecipeModel.java
+++ app/src/main/java/com/example/rezepteapp/model/RecipeModel.java
@@ -4,10 +4,7 @@ import android.content.Context;
 
 import com.example.rezepteapp.daos.IngredientDAOImpl;
 import com.example.rezepteapp.daos.ShoppinglistDAOImpl;
-import com.example.rezepteapp.database.RecipeDbOpenHelper;
-import com.example.rezepteapp.mapper.ToShoppingListMapper;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RecipeModel {
@@ -19,8 +16,7 @@ public class RecipeModel {
 
     public List<ShoppinglistEntry> getToDoList() {
         toDoList.clear();
-        ToShoppingListMapper mapper = new ToShoppingListMapper();
-        toDoList.addAll(mapper.mapToList(sldao.getAllShoppinglist(), idao.getAllIngredients()));
+//        toDoList.addAll(mapper.mapToList(sldao.getAllShoppinglist(), idao.getAllIngredients()));
 
         return toDoList;
     }
