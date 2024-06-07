d786826f8f849af439d22acac2bcc3536711391d
Resi-r <teresa.rehers@gmail.com>
2024-06-07 22:19:52 +0200

    changed test data

--- app/src/main/java/com/example/rezepteapp/controller/WelcomeScreenFragment.java
+++ app/src/main/java/com/example/rezepteapp/controller/WelcomeScreenFragment.java
@@ -30,7 +30,6 @@ public class WelcomeScreenFragment extends Fragment implements WeatherCallback {
     private FragmentWelcomeScreenBinding binding;
     private WeatherCall weatherCall;
     private List<Recipe> recipeList;
-    private List<Recipe> testList;
     private WelcomeRecipeListAdapter welcomeRecipeListAdapter;
 
     @Override
@@ -58,22 +57,6 @@ public class WelcomeScreenFragment extends Fragment implements WeatherCallback {
         getWeather();
         this.recipeList = model.getDailyRecipes();
 
-        testList = new ArrayList<>();
-
-        testList.add(new Recipe("Hallo",  null, "1h", "2h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Tschüss",  null, "1h", "1h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Haha",  null, "1h", "5h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Lol",  null, "1h", "3h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Hallo1",  null, "1h", "2h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Tschüss1",  null, "1h", "1h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Haha1",  null, "1h", "5h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Lol1",  null, "1h", "3h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Hallo2",  null, "1h", "2h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Tschüss2",  null, "1h", "1h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Haha2",  null, "1h", "5h", 4, null, null, null, Status.LIVE));
-        testList.add(new Recipe("Lol2",  null, "1h", "3h", 4, null, null, null, Status.LIVE));
-
-        //welcomeRecipeListAdapter = new WelcomeRecipeListAdapter(randomizeRecipeSuggestions(testList));
         welcomeRecipeListAdapter = new WelcomeRecipeListAdapter(randomizeRecipeSuggestions(recipeList));
 
         binding.recyclView.setAdapter(welcomeRecipeListAdapter);
@@ -84,7 +67,7 @@ public class WelcomeScreenFragment extends Fragment implements WeatherCallback {
     }
 
     private void reloadRecipes() {
-        welcomeRecipeListAdapter.updateRecipes(randomizeRecipeSuggestions(testList));
+        welcomeRecipeListAdapter.updateRecipes(randomizeRecipeSuggestions(recipeList));
     }
 
     public ArrayList<Recipe> randomizeRecipeSuggestions(List<Recipe> list) {
